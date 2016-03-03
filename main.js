ee813f4ab89d1e1a57b4d7d9d32902e1c00ef1df
laiyanlong <laiyanlong@gmail.com>
2016-03-04 00:28:09 +0800

    Clear return information

--- main.js
+++ main.js
@@ -29,15 +29,30 @@ curl.on( 'end', function( statusCode, body, headers ) {
     var fruits = [];
     var newTitles = [];
     var newHrefs = [];
+    var newTimes = [];
+    var titles = [];
+    var hrefs = [];
+    var images = [];
     var comic_num = 0;
     $('li').each(function(i, elem) {
         var href = $(this).find('a').attr('href');
         if(href !== undefined){
-          fruits[comic_num] = $(this).text();
-          newTitles[comic_num] = $(this).find('a').text();
-          newHrefs[comic_num] = $(this).find('a').attr('href');
-          console.info("i: "+i+", elem: "+$(this)+ ", newTitles: " + newTitles[comic_num]+ ", newHrefs: " + newHrefs[comic_num]);
-          comic_num++;
+            fruits[comic_num] = $(this).text();
+            newTitles[comic_num] = $(this).find('a').text();
+            newHrefs[comic_num] = $(this).find('a').attr('href');
+            newTimes[comic_num] = $(this).find('span').text();
+            titles[comic_num] = $(this).find('a').attr('title');
+            hrefs[comic_num] = $(this).find('a').attr('href');
+            images[comic_num] = $(this).find('img').attr('src');
+        if(newTitles[comic_num]!==""){
+            console.info("newTitles: " + newTitles[comic_num]+ ", newHrefs: " + newHrefs[comic_num] + ", newTimes: " + newTimes[comic_num]);
+            comic_num++;
+          }
+        else {
+            // console.info("i: "+i+", elem: "+$(this)+ ", titles: " + titles[comic_num]+ ", hrefs: " + hrefs[comic_num] + ", images: " + images[comic_num]);
+            console.info("titles: " + titles[comic_num]+ ", hrefs: " + hrefs[comic_num] + ", images: " + images[comic_num]);
+            comic_num++;
+          }
         }
     });
     //fruits.join(', ');
